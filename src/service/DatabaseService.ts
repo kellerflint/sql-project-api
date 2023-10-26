7ae9ea2e209428440e52631ef9ab56d8b512251e
James Motherwell <motherwell.james@student.greenriver.edu>
2023-10-26 16:36:26 -0700

    Import mssql instead of storing it as a class field

--- src/service/DatabaseService.ts
+++ src/service/DatabaseService.ts
@@ -1,20 +1,14 @@
+import sql from "mssql";
+
 import { getDbConfig } from "./ConfigService";
 
 export default class DatabaseConnection {
-    sql: any; 
-
-    constructor() {
-        this.sql = require("mssql");
-    }
-
     async connect() {
         try {
-            await this.sql.connect(getDbConfig());
+            await sql.connect(getDbConfig());
             console.log("Successful");
         } catch (error) {
             console.log(error);
         }
     }
-
-
 }
